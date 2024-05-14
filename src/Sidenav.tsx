eca726a9cee1d778b004b69a8ff566c21bcefa31
JuanJhonv <valdiviesofernandezj@gmail.com>
2024-05-14 13:06:39 -0500

    Avance scroll, animation background

--- src/Sidenav.tsx
+++ src/Sidenav.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useRef,useEffect } from "react"
 export function Sidenav(){
   const scroll_sidenav = useRef();
   const onscroll = (e)=>{
@@ -8,19 +8,35 @@ export function Sidenav(){
   const mouseenter = (e)=>{
     // e.target.style.height = `${scroll_sidenav.current.clientHeight}`
     // scroll_sidenav.current.style.height = `${(scroll_sidenav.current.clientHeight/scroll_sidenav.current.scrollHeight) * e.target.clientHeight}px`
-    console.log(e.target,e.target.clientHeight,e.target.scrollHeight)
+    // console.log(e.target,e.target.clientHeight,e.target.scrollHeight)
+    if(e.target.matches('.pepe')){
+      // scroll_sidenav.current.style.height = `${(scroll_sidenav.current.clientHeight/scroll_sidenav.current.scrollHeight) * e.target.clientHeight}px`
+      scroll_sidenav.current.style.height = `${(e.target.clientHeight/e.target.scrollHeight) * e.target.clientHeight}px`
+      scroll_sidenav.current.style.visibility = 'visible'
+      console.log(e.target.clientHeight,e.target.scrollHeight,scroll_sidenav.current.clientHeight)
+      // console.log(scroll_sidenav.current.clientHeight,e.target.clientHeight)
+    }
+    // console.log(e.target.clientHeight)
+    // console.log(scroll_sidenav.current.style.height)
   }
+  const onanimationend = (e)=>{
+
+  }
+  useEffect(()=>{
+
+  },[])
   return(
     <>
       <nav 
         className="flex flex-col text-[12px] flex-none gap-y-3 w-[15%] bg-[white] overflow-hidden relative"
       >
         <div 
-          className="block w-[6px] rounded-xl right-0 absolute bg-gray-400 h-[50%]"
+          className="block w-[6px] rounded-xl right-0 absolute bg-gray-400 h-[50%] invisible"
           ref={scroll_sidenav}
+          onAnimationEnd={onanimationend}
         ></div>
         <div 
-          className="flex flex-col gap-3 flex-1 overflow-y-scroll min-w-[100%] mr-[-17px] p-3"
+          className="flex flex-col gap-3 flex-1 overflow-y-scroll min-w-[100%] mr-[-17px] p-3 pepe"
           onScroll={onscroll}
           onMouseEnter={mouseenter}
         >
