c093be45f4f3741085bedb6990d32c0fff755c5e
juanpablov <valdiviesofernandezj@gmail.com>
2024-07-02 01:02:24 -0500

    Subida de archivos al server

--- src/Directory.tsx
+++ src/Directory.tsx
@@ -75,17 +75,9 @@ const contextual_content = {
         <li>
           <div className='flex gap-4 p-2 select-none cursor-pointer w-[100%]'>
             <div className="pl-1">
-              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-folder-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 19h-7a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v3.5" /><path d="M16 19h6" /><path d="M19 16v6" /></svg>
             </div>
-            <div className='flex flex-col items-center text-left'>Descargar</div>
-          </div>
-        </li>
-        <li>
-          <div className='flex gap-4 p-2 select-none cursor-pointer w-[100%]'>
-            <div className="pl-1">
-              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-pencil-minus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /><path d="M16 19h6" /></svg>
-            </div>
-            <div className='flex flex-col items-center text-left'>Cambiar nombre</div>
+            <div className='flex flex-col items-center text-left'>Nueva Carpeta</div>
           </div>
         </li>
       </ul>
@@ -94,17 +86,17 @@ const contextual_content = {
         <li>
           <div className='flex gap-4 p-2 select-none cursor-pointer w-[100%]'>
             <div className="pl-1">
-              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user-share"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h3" /><path d="M16 22l5 -5" /><path d="M21 21.5v-4.5h-4.5" /></svg>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-file-upload"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2z" /><path d="M12 11v6" /><path d="M9.5 13.5l2.5 -2.5l2.5 2.5" /></svg>
             </div>
-            <div className='flex flex-col items-center text-left'>Compartir</div>
+            <div className='flex items-center text-left flex-1' data-action='subir'>Subir archivo</div>
           </div>
         </li>
         <li>
           <div className='flex gap-4 p-2 select-none cursor-pointer w-[100%]'>
             <div className="pl-1">
-              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-folder-open"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 19l2.757 -7.351a1 1 0 0 1 .936 -.649h12.307a1 1 0 0 1 .986 1.164l-.996 5.211a2 2 0 0 1 -1.964 1.625h-14.026a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v2" /></svg>
+              <svg  xmlns="http://www.w3.org/2000/svg"  width="20"  height="20"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-folder-up"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 19h-7a2 2 0 0 1 -2 -2v-11a2 2 0 0 1 2 -2h4l3 3h7a2 2 0 0 1 2 2v3.5" /><path d="M19 22v-6" /><path d="M22 19l-3 -3l-3 3" /></svg>
             </div>
-            <div className='flex flex-col items-center text-left'>Organizar</div>
+            <div className='flex flex-col items-center text-left'>Subir carpeta</div>
           </div>
         </li>
         <li>
@@ -153,22 +145,22 @@ function Carpeta({name,ondoubleclick}){
           <ul className="flex flex-row justify-end">
             <li>
               <div className="rounded-full w-9 h-9 hover:bg-gray-300 transition-colors flex justify-center items-center">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="15"  height="15"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
               </div>
             </li>
             <li>
               <div className="rounded-full w-9 h-9 hover:bg-gray-300 transition-colors flex justify-center items-center">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="15"  height="15"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-download"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" /><path d="M7 11l5 5l5 -5" /><path d="M12 4l0 12" /></svg>
               </div>
             </li>
             <li>
               <div className="rounded-full w-9 h-9 hover:bg-gray-300 transition-colors flex justify-center items-center">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="15"  height="15"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-pencil-minus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /><path d="M16 19h6" /></svg>
               </div>
             </li>
             <li>
               <div className="rounded-full w-9 h-9 hover:bg-gray-300 transition-colors flex justify-center items-center">
-                <svg  xmlns="http://www.w3.org/2000/svg"  width="15"  height="15"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-user-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M16 19h6" /><path d="M19 16v6" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4" /></svg>
+                <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-star"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" /></svg>
               </div>
             </li>
           </ul>
@@ -201,11 +193,6 @@ async function Consulta(url,method = 'GET'){
     console.log('sdofo')
     // console.log(error)
   }
-  // const onclick = (e)=>{
-  //   if(e.target.matches("tr")){
-  //     e.target.focus()
-  //   }
-  // }
 }
 
 export function Directory(){
@@ -216,14 +203,38 @@ export function Directory(){
 
   const onrightclick = (e)=>{
     e.preventDefault()
+    const content = e.target.matches('div') ? contextual_content.context2 : contextual_content.context1
+    const actions = 
+    e.target.matches('div') 
+    ? (e)=>{
+      if(e.target.matches("div[data-action='subir'")){
+        const filebox = document.createElement('input')
+        filebox.type = 'file'
+        filebox.click()
+        filebox.addEventListener('change',(ec)=>{
+          async function subirfile(){
+            try {
+              const formdata = new FormData()
+              formdata.append('file',filebox.files[0])
+              await fetch("http://localhost:4000/directorio/postFile",{
+                method:'POST',
+                body: formdata
+              })
+            } catch (error) {
+              console.log(error)
+            }
+          }
+          subirfile()
+        })
+      }
+    }
+    : (e)=>{
+      console.log(e.target)
+    }
     open({
       position:{x:e.clientX,y:e.clientY},
-      content: contextual_content.context1,
-      actions: {
-        descargar: (e)=>{
-          alert("Descargando")
-        }
-      }
+      content: content,
+      actions: actions
     })
   }
   const ondoubleclick = async (e)=>{
--- src/components/ContextMenu/ContextualMenu.tsx
+++ src/components/ContextMenu/ContextualMenu.tsx
@@ -1,20 +1,18 @@
 import { useEffect, useRef } from "react"
 
 export function ContextualMenu({params}){
-  const context_menu = useRef()
+  const context_menu = useRef(null)
   const onclickright = (e)=>{
     e.preventDefault()
   }
   useEffect(()=>{
-    console.log(params.actions.descargar)
-    context_menu.current.style.left = `${params.position.x}px`
-    context_menu.current.style.top = `${params.position.y}px`
+    context_menu.current.style.left = `${params.position.x + 320 > window.innerWidth ? params.position.x - 320 : params.position.x}px`
+    context_menu.current.style.top = `${params.position.y + 300 > window.innerHeight ? params.position.y - 300 : params.position.y}px`
     context_menu.current.classList.remove('invisible')
     context_menu.current.focus()
-    console.log('termino el renderizado')
   },[params])
   return(
-    <div ref={context_menu} onClick={params.actions.descargar} onContextMenu={onclickright} className="config w-[320px] h-[300px] pt-2 pb-2 bg-white rounded-md absolute shadow-lg shadow-gray-400/50 border flex flex-col gap-2 left-[100px] invisible opacity-0 z-[-1] focus:opacity-100 focus:z-50 transition-opacity" tabIndex={-1}>  
+    <div ref={context_menu} onClick={params.actions} onContextMenu={onclickright} className="config w-[320px] h-[300px] pt-2 pb-2 bg-white rounded-md absolute shadow-lg shadow-gray-400/50 border flex flex-col gap-2 left-[100px] invisible opacity-0 z-[-1] focus:opacity-100 focus:z-50 transition-opacity" tabIndex={-1}>  
       {
         params.content
       }
