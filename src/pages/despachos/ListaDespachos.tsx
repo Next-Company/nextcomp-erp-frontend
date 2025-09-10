81959a63b4e3f41d4db4cfe02a567b823b4fb29e
JuanJhonv <valdiviesofernandezj@gmail.com>
2025-09-09 19:29:05 -0500

    Mejoras guias ingreso despachos y servicios

--- src/pages/despachos/ListaDespachos.tsx
+++ src/pages/despachos/ListaDespachos.tsx
@@ -202,7 +202,8 @@ export default function ListaDespachos() {
               })
           }
         }
-        openModal(params_modal)
+        console.log("Los params son:",params_modal)
+        // openModal(params_modal)
         break;
       case 'download':
         params_modal = {
@@ -510,7 +511,7 @@ export default function ListaDespachos() {
                           <td className="w-[250px]">
                             <ul className="flex flex-row justify-end">
                               <li>
-                                <div className="rounded-full w-9 h-9 hover:bg-gray-100 transition-colors flex justify-center items-center" data-action="delete" onClick={onclick} data-id={row.idx} data-despacho={row.distribucion}>
+                                <div className="rounded-full w-9 h-9 hover:bg-gray-100 transition-colors flex justify-center items-center" data-action="delete" onClick={onclick} data-id={row.idx} data-distribucion={row.distribucion}>
                                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-trash"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M4 7l16 0" /><path d="M10 11l0 6" /><path d="M14 11l0 6" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                                 </div>
                               </li>
